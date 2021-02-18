f0a1adf1dc75a1917c1fc4dee91f20b79d2a7550
saudrey <weerawutearth@gmail.com>
2021-02-18 15:53:29 +0700

    Pet

---
+++ 6220502183_3_1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+{
+  int a[5][4],i,j;
+
+  for ( i = 0; i <5; i++)
+  {
+      for ( j = 0; j < 4; j++)
+      {
+          scanf("%d",&a[i][j]);
+      }
+      
+  }
+
+  
+     
+}
